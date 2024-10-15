540bf8d87defcf6aec119bc1f6e295b6d89166c2
Vitor <vi7org@hotmail.com>
2024-10-14 22:41:09 -0400

    Modify assert equal to be able to compare arrays

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,20 @@
 const assertEqual = function(actual, expected) {
+  if (Array.isArray(actual) && Array.isArray(expected)) {
+    if (actual.length !== expected.length) {
+      console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+      return;
+    }
+
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+        return;
+      }
+    }
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+    return;
+  }
+
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
   } else {
@@ -11,3 +27,7 @@ assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1, 1);
 assertEqual(1, 2);
+assertEqual([1, 2, 3], [1, 2, 3]);
+assertEqual([1, 2, 3], [3, 2, 1]);
+assertEqual([1, 2, 3], [1, 2, 3, 4]);
+assertEqual([1, 2, 3], [1, 2]);
