08d17f9096bd50a3a3c9110586e877b2573e2219
Vitor <vi7org@hotmail.com>
2024-10-14 22:50:52 -0400

    Add more test cases for tail

--- tail.js
+++ tail.js
@@ -19,3 +19,8 @@ assertEqual(result1[1], 7);
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+const result2 = tail([]);
+assertEqual(result2.length, 0);
+assertEqual(result2[0], undefined);
+const result3 = tail([1]);
+assertEqual(result3.length, 0);
