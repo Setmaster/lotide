3fd28607958c5cee59066a79109f7ac2ec9cece8
Vitor <vi7org@hotmail.com>
2024-10-16 14:11:28 -0400

    Refactored eqObjects for better readability. Updated conditionals in comparison function to check if the object is not an array before comparing its values.

--- eqObjects.js
+++ eqObjects.js
@@ -28,10 +28,11 @@ const eqObjects = function (object1, object2) {
         return false;
     }
 
-    for (const key of Object.keys(obj1Keys)) {
+    for (const key of obj1Keys) {
         if (Array.isArray(object1[key]) && eqArrays(object1[key], object2[key]) === false) {
             return false;
-        } else if (object1[key] !== object2[key]) {
+        }
+        if (!Array.isArray(object1[key]) && object1[key] !== object2[key]) {
             return false;
         }
     }
@@ -45,8 +46,8 @@ assertEqual(eqObjects(shirtObject, anotherShirtObject), true)
 const longSleeveShirtObject = {size: "medium", color: "red", sleeveLength: "long"};
 assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
 
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+const multiColorShirtObject = {colors: ["red", "blue"], size: "medium"};
+const anotherMultiColorShirtObject = {size: "medium", colors: ["red", "blue"]};
 assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
 
 const longSleeveMultiColorShirtObject = {
