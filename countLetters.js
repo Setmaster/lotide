9d330e25087275dc1857569caf6aecfc6ca498a7
Vitor <vi7org@hotmail.com>
2024-10-16 13:14:52 -0400

    Make countLetters ignore spaces

--- countLetters.js
+++ countLetters.js
@@ -1,25 +1,25 @@
-﻿const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
-const countLetters = function(str) {
-  let output = {};
-
-  for (const char of str) {
-    if (!output.hasOwnProperty(char)) {
-      output[char] = 1;
+﻿const assertEqual = function (actual, expected) {
+    if (actual === expected) {
+        console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
     } else {
-      output[char] += 1;
+        console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
     }
-  }
-  return output;
 };
 
-let result1 = countLetters("potato");
+const countLetters = function (str) {
+    let output = {};
+    str = str.replace(' ', '') //ignore spaces
+    for (const char of str) {
+        if (!output.hasOwnProperty(char)) {
+            output[char] = 1;
+        } else {
+            output[char] += 1;
+        }
+    }
+    return output;
+};
+
+let result1 = countLetters("pota to");
 
 assertEqual(result1.p, 1);
 assertEqual(result1.o, 2);
