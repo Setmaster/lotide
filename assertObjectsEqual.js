e2a6ada41a6a55b274c63801e9653b6e94b09371
Vitor <vi7org@hotmail.com>
2024-10-17 12:56:11 -0400

    Add map function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,67 +1,67 @@
-﻿const eqArrays = function (array1, array2) {
-    if (array1.length !== array2.length) {
-        return false;
+﻿const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
-    for (let i = 0; i < array1.length; i++) {
-        if (array1[i] !== array2[i]) {
-            return false;
-        }
-    }
-    return true;
+  }
+  return true;
 };
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function (object1, object2) {
-    const obj1Keys = Object.keys(object1);
-    const obj2Keys = Object.keys(object2);
+const eqObjects = function(object1, object2) {
+  const obj1Keys = Object.keys(object1);
+  const obj2Keys = Object.keys(object2);
 
-    if (obj1Keys.length !== obj2Keys.length) {
-        return false;
-    }
+  if (obj1Keys.length !== obj2Keys.length) {
+    return false;
+  }
 
-    for (const key of obj1Keys) {
-        if (Array.isArray(object1[key]) && eqArrays(object1[key], object2[key]) === false) {
-            return false;
-        }
-        if (!Array.isArray(object1[key]) && object1[key] !== object2[key]) {
-            return false;
-        }
+  for (const key of obj1Keys) {
+    if (Array.isArray(object1[key]) && eqArrays(object1[key], object2[key]) === false) {
+      return false;
     }
-    return true;
+    if (!Array.isArray(object1[key]) && object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
 };
 
-const assertObjectsEqual = function (actual, expected) {
-    const inspect = require("util").inspect;
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require("util").inspect;
     
-    if (eqObjects(actual, expected)) {
-        console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
-    } else {
-        console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
-    }
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
 };
 
 const obj1 = {
-    potato: "plant",
-    classNumber: 430,
-    sizes: [42, 34, 25]
+  potato: "plant",
+  classNumber: 430,
+  sizes: [42, 34, 25]
 };
 
 const obj2 = {
-    potato: "plant",
-    classNumber: 430,
-    sizes: [42, 34, 25]
+  potato: "plant",
+  classNumber: 430,
+  sizes: [42, 34, 25]
 };
 
 const obj3 = {
-    potato: "plant",
-    classNumber: 430
+  potato: "plant",
+  classNumber: 430
 };
 
 const obj4 = {
-    classNumber: 430,
-    sizes: [42, 34, 25],
-    potato: "plant"
+  classNumber: 430,
+  sizes: [42, 34, 25],
+  potato: "plant"
 };
 
 assertObjectsEqual(obj1, obj2); // should pass
--- eqObjects.js
+++ eqObjects.js
@@ -1,47 +1,47 @@
-﻿const assertEqual = function (actual, expected) {
-    if (actual === expected) {
-        console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-    } else {
-        console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-    }
+﻿const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
 };
 
-const eqArrays = function (array1, array2) {
-    if (array1.length !== array2.length) {
-        return false;
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
-    for (let i = 0; i < array1.length; i++) {
-        if (array1[i] !== array2[i]) {
-            return false;
-        }
-    }
-    return true;
+  }
+  return true;
 };
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function (object1, object2) {
-    const obj1Keys = Object.keys(object1);
-    const obj2Keys = Object.keys(object2);
+const eqObjects = function(object1, object2) {
+  const obj1Keys = Object.keys(object1);
+  const obj2Keys = Object.keys(object2);
 
-    if (obj1Keys.length !== obj2Keys.length) {
-        return false;
-    }
+  if (obj1Keys.length !== obj2Keys.length) {
+    return false;
+  }
 
-    for (const key of obj1Keys) {
-        if (Array.isArray(object1[key]) && eqArrays(object1[key], object2[key]) === false) {
-            return false;
-        }
-        if (!Array.isArray(object1[key]) && object1[key] !== object2[key]) {
-            return false;
-        }
+  for (const key of obj1Keys) {
+    if (Array.isArray(object1[key]) && eqArrays(object1[key], object2[key]) === false) {
+      return false;
     }
-    return true;
+    if (!Array.isArray(object1[key]) && object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const shirtObject = {color: "red", size: "medium"};
 const anotherShirtObject = {size: "medium", color: "red"};
-assertEqual(eqObjects(shirtObject, anotherShirtObject), true)
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
 
 const longSleeveShirtObject = {size: "medium", color: "red", sleeveLength: "long"};
 assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
@@ -51,9 +51,9 @@ const anotherMultiColorShirtObject = {size: "medium", colors: ["red", "blue"]};
 assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
 
 const longSleeveMultiColorShirtObject = {
-    size: "medium",
-    colors: ["red", "blue"],
-    sleeveLength: "long",
+  size: "medium",
+  colors: ["red", "blue"],
+  sleeveLength: "long",
 };
 
 assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
