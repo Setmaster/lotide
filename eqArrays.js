b9233769caf68af50eaa8878e6a827e883627a96
Vitor <vi7org@hotmail.com>
2024-10-19 13:42:59 -0400

    Make eqArrays recursive

--- eqArrays.js
+++ eqArrays.js
@@ -11,7 +11,11 @@ const eqArrays = function(array1, array2) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+    if (Array.isArray(array1[i]) && !eqArrays(array1[i], array2[i])){
+      return false;
+    }
+    
+    if (!Array.isArray(array1[i]) && array1[i] !== array2[i]) {
       return false;
     }
   }
@@ -23,3 +27,8 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
