620a116a89f8ee9334d97d78bf7028fc0b76b269
Vitor <vi7org@hotmail.com>
2024-10-21 13:30:29 -0400

    Add middle test

--- middle.js
+++ middle.js
@@ -1,7 +1,4 @@
-﻿const assertArraysEqual = require("./assertArraysEqual");
-
-
-const middle = function(array) {
+﻿const middle = function(array) {
   if (array.length < 3) {
     return [];
   }
@@ -11,9 +8,4 @@ const middle = function(array) {
   return [array[Math.ceil((array.length / 2)) - 1], array[(Math.ceil((array.length / 2)))]];
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
