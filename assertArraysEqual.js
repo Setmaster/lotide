ba6c6a25ca696a050bdc13f6912a3bef3c590cc1
Vitor <vi7org@hotmail.com>
2024-10-21 13:27:47 -0400

    Create test for assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -9,9 +9,3 @@ const assertArraysEqual = function(array1, array2) {
 };
 
 module.exports = assertArraysEqual;
-
-// Tests
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // should pass
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // should fail
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // should pass
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // should fail
