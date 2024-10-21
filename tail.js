1ee5f841b6b4804cef3f78a07fa8af8234554f2c
Vitor <vi7org@hotmail.com>
2024-10-21 13:24:30 -0400

    Export tail, create test for tail

--- tail.js
+++ tail.js
@@ -1,19 +1,5 @@
-﻿const assertEqual = require("./assertEqual");
-
-const tail = function(array) {
+﻿const tail = function(array) {
   return array.slice(1);
 };
 
-// Test cases
-const result1 = tail([5,6,7]);
-assertEqual(result1.length, 2);
-assertEqual(result1[0], 6);
-assertEqual(result1[1], 7);
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-const result2 = tail([]);
-assertEqual(result2.length, 0);
-assertEqual(result2[0], undefined);
-const result3 = tail([1]);
-assertEqual(result3.length, 0);
+module.exports = tail;
